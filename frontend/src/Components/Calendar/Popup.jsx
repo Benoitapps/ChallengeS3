627b4b757cd56d16be67d0034ef7141423489996
MelvinCourant <melvin.courant1@gmail.com>
2023-11-09 15:22:31 +0100

    :lipstick: add style to schedule and it's popup

--- frontend/src/Components/Calendar/Popup.jsx
+++ frontend/src/Components/Calendar/Popup.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../assets/css/Popup.css';
+import '@css/Popup.css';
 
 const Popup = ({ show, children, onClose, onOk }) => {
     if (!show) return null;
@@ -13,13 +13,15 @@ const Popup = ({ show, children, onClose, onOk }) => {
 
     return (
         <div className="popup">
-            <div className="popup-content">
-                <span className="close" onClick={onClose}>
-                    &times;
-                </span>
+            <div className="popup__content">
+                <svg className="close" onClick={onClose} width="35" height="35" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M18 7.05 16.95 6 12 10.95 7.05 6 6 7.05 10.95 12 6 16.95 7.05 18 12 13.05 16.95 18 18 16.95 13.05 12 18 7.05Z"></path>
+                </svg>
                 {children}
-                <button onClick={handleOk}>Ok</button>
-                <button onClick={onClose}>Annuler</button>
+                <div className="popup__content__buttons">
+                    <button onClick={handleOk} className="primary-button">Réserver</button>
+                    <button onClick={onClose}>Annuler</button>
+                </div>
             </div>
         </div>
     );
--- frontend/src/Components/Calendar/Schedule.jsx
+++ frontend/src/Components/Calendar/Schedule.jsx
@@ -5,8 +5,7 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import listPlugin from '@fullcalendar/list'
 import PopUp from "./Popup.jsx";
-import '../../assets/css/App.css';
-import '../../assets/css/Schedule.css';
+import '@css/Schedule.css';
 import {tab} from './events.jsx';
 
 
@@ -36,7 +35,10 @@ export default class Schedule extends React.Component {
         const minutes = date.getUTCMinutes();
         const seconds = date.getUTCSeconds();
 
-        return `${day} ${month} ${year}, ${hours}:0${minutes}`;
+        return {
+            date: `${day}/${month}/${year}`,
+            time: `${hours}:0${minutes} à ${hours + 1}:0${minutes}`,
+        }
     }
 
 
@@ -47,9 +49,23 @@ export default class Schedule extends React.Component {
         let DateFormat = this.formatReadableDate(arg.dateStr);
 
         const modalContent = (
-            <div>
-                <h2>Voulez vous prendre le crenaux du :</h2>
-                <p>{DateFormat}</p>
+            <div className="popup__content__texts">
+                <h2>Réserver ce créneau</h2>
+                <ul className="popup__content__texts__datetime">
+                    <li>
+                        <svg width="30" height="30" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M19.5 3h-3V1.5H15V3H9V1.5H7.5V3h-3C3.675 3 3 3.675 3 4.5v15c0 .825.675 1.5 1.5 1.5h15c.825 0 1.5-.675 1.5-1.5v-15c0-.825-.675-1.5-1.5-1.5Zm0 16.5h-15V9h15v10.5Zm0-12h-15v-3h3V6H9V4.5h6V6h1.5V4.5h3v3Z"></path>
+                        </svg>
+                        <p>{DateFormat.date}</p>
+                    </li>
+                    <li>
+                        <svg width="30" height="30" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 22.5a10.5 10.5 0 1 1 0-21 10.5 10.5 0 0 1 0 21ZM12 3a9 9 0 1 0 0 18 9 9 0 0 0 0-18Z"></path>
+                            <path d="m15.443 16.5-4.193-4.193V5.25h1.5v6.435l3.75 3.758-1.057 1.057Z"></path>
+                        </svg>
+                        <p>{DateFormat.time}</p>
+                    </li>
+                </ul>
             </div>
         );
 
@@ -57,7 +73,6 @@ export default class Schedule extends React.Component {
             isModalOpen: true,
             modalContent: modalContent,
         });
-        console.log("modalConetent", modalContent);
     };
 
     closeModal = () => {
@@ -71,44 +86,36 @@ export default class Schedule extends React.Component {
 
         return (
             <>
-                <main>
-
-                <div className="calendar">
-            <FullCalendar
-                plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin,listPlugin ]}
-                initialView={'timeGridWeek'}
-                slotDuration="01:00:00"
-                events={this.state.events}
-                allDaySlot={false}
-                editable={true}
-                selectable={true}
-                slotMinTime="05:00:00"
-                slotMaxTime="22:00:00"
-                // contentHeight="200px"
-                headerToolbar={
-                    {
-                       start : "today prev,next",
-                        center: 'title',
-                        end: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-                    }
-                }
-                businessHours={{
-                    daysOfWeek: [1, 2, 3, 4, 5, 6],
-                    startTime: '08:00',
-                    endTime: '17:00',
-                }}
-                height={"31em"}
-                locale={"fr"}
-                dateClick={(e) => this.handleDateClick(e)}
-            />
-                </div>
-                <div className="flex">
-                <div className="popup">
+                <main class="schedule">
+                    <FullCalendar
+                        plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin,listPlugin ]}
+                        initialView={'timeGridWeek'}
+                        slotDuration="01:00:00"
+                        events={this.state.events}
+                        allDaySlot={false}
+                        editable={true}
+                        selectable={true}
+                        slotMinTime="05:00:00"
+                        slotMaxTime="22:00:00"
+                        headerToolbar={
+                            {
+                               start : "today prev,next",
+                                center: 'title',
+                                end: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+                            }
+                        }
+                        businessHours={{
+                            daysOfWeek: [1, 2, 3, 4, 5, 6],
+                            startTime: '08:00',
+                            endTime: '17:00',
+                        }}
+                        height={"31.7em"}
+                        locale={"fr"}
+                        dateClick={(e) => this.handleDateClick(e)}
+                    />
                     <PopUp show={this.state.isModalOpen} onClose={this.closeModal}>
                         {this.state.modalContent}
                     </PopUp>
-                </div>
-                </div>
                 </main>
             </>
 
