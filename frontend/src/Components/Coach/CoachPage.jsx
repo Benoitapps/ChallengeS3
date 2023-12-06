e23e026ed4e44242cd76601c913354db36261445
MelvinCourant <melvin.courant1@gmail.com>
2023-12-06 11:58:13 +0100

    :children_crossing: Update sytem of vote

--- frontend/src/Components/Coach/CoachPage.jsx
+++ frontend/src/Components/Coach/CoachPage.jsx
@@ -1,18 +1,15 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getCoachDetails } from "../../hook/coach/getCoach.js";
-import { addReview } from "../../hook/coach/addReview.js";
-import { getUserId } from "../User/DecodeUser.jsx";
 import CoachProfile from "./CoachProfile";
 import CoachContent from "./CoachContent";
+import CoachAddReview from "./CoachAddReview";
 import '@css/Coach.css';
 
 function CoachPage() {
     const { id } = useParams();
     const [coach, setCoach] = useState({});
     const [loading, setLoading] = useState(true);
-    const [success, setSuccess] = useState(false);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
         const getCoach = async () => {
@@ -24,25 +21,6 @@ function CoachPage() {
         getCoach();
     }, []);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const data = new FormData(e.target);
-
-        if(
-          data.get('note') > 0 ||
-          data.get('note') < 6
-        ) {
-            const user = await getUserId();
-            const sendReview = await addReview(id, user, parseInt(data.get('note')));
-
-            if (sendReview.status === 201) {
-                setSuccess(true);
-            } else {
-                setError('Une erreur est survenue');
-            }
-        }
-    };
-
     return (
         <main>
             {
@@ -53,17 +31,7 @@ function CoachPage() {
                         <CoachProfile coach={coach}/>
                         <div className="coach-content">
                             <CoachContent coach={coach} id={id}/>
-
-                            <form className="coach-review" onSubmit={(e) => handleSubmit(e)}>
-                                <input type="number" name="note" min="1" max="5"/>
-                                <input type="submit" value="Noter"/>
-                            </form>
-                            {
-                                success && <p className="success">Votre note a bien été prise en compte</p>
-                            }
-                            {
-                                error && <p className="error">{error}</p>
-                            }
+                            <CoachAddReview id={id}/>
                         </div>
                     </div>
             }
