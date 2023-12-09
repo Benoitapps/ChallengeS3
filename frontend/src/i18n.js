24c677dfabd6e4ec8d642c3cde831ae041f71364
xxcolas <colas.capel73@gmail.com>
2023-12-09 22:57:56 +0000

    indent i18n initialization and add build target

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,7 +1,6 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-
 import { translate } from "../src/services/traductionReForm.js";
 
 const chargerTraductions = async (langue) => {
@@ -16,30 +15,31 @@ const chargerTraductions = async (langue) => {
         return { translations: {} };
     }
 };
+
 let tradFR = await chargerTraductions("fr");
 let tradEN = await chargerTraductions("en");
 
-    i18n
-        .use(LanguageDetector)
-        .use(initReactI18next)
-        .init({
-            resources: {
-                en: {
-                    translations: tradEN,
-                }, // Resolve the promise here
-                fr: {
-                    translations: tradFR
-                }
-            },
-            fallbackLng: "fr",
-            debug: false,
-            ns: ["translations"],
-            defaultNS: "translations",
-            keySeparator: false,
-            interpolation: {
-                escapeValue: false
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources: {
+            en: {
+                translations: tradEN,
+            }, // Resolve the promise here
+            fr: {
+                translations: tradFR
             }
-        });
+        },
+        fallbackLng: "fr",
+        debug: false,
+        ns: ["translations"],
+        defaultNS: "translations",
+        keySeparator: false,
+        interpolation: {
+            escapeValue: false
+        }
+    });
 
 
 export default i18n;
