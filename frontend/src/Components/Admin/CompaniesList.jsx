621be75314812391c97ef9d5368fe30edf5ad254
xxcolas <colas.capel73@gmail.com>
2024-01-23 18:07:12 +0000

    admin: ajout de la modification d'une entreprise

--- frontend/src/Components/Admin/CompaniesList.jsx
+++ frontend/src/Components/Admin/CompaniesList.jsx
@@ -6,6 +6,9 @@ function CompaniesList() {
     const [companies, setCompanies] = useState([]);
     const [companiesLoading, setCompaniesLoading] = useState(false);
 
+    const [beingEdited, setBeingEdited] = useState(false);
+    const [currentCompanyId, setCurrentCompanyId] = useState(null);
+
     useEffect(() => {
         const loadData = async () => {
             setCompaniesLoading(true);
@@ -46,6 +49,41 @@ function CompaniesList() {
         setCompaniesLoading(false);
     };
 
+    const onEdit = (companyId) => {
+        setBeingEdited(!beingEdited);
+        setCurrentCompanyId(companyId);
+    };
+
+    const onSave = async (companyId) => {
+        setCompaniesLoading(true);
+
+        let companyInputs = document.querySelectorAll(`input[name="name"], textarea[name="description"]`);
+        let company = {};
+        companyInputs.forEach(input => company[input.name] = input.value);
+
+        let result = await fetch(`${env.VITE_URL_BACK}/api/companies/` + companyId, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/merge-patch+json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token'),
+            },
+            body: JSON.stringify(company),
+        });
+        result = await result.json();
+        
+        if (!result) return alert('Erreur lors de la modification de la company');
+
+        let companyIndex = companies.findIndex(company => company.id === companyId);
+        companies[companyIndex].name = company.name;
+        companies[companyIndex].description = company.description;
+
+        setCompanies([...companies])
+
+        setCompaniesLoading(false);
+        setBeingEdited(!beingEdited);
+        setCurrentCompanyId(null);
+    };
+
     return (
         <main>
             {companiesLoading 
@@ -66,25 +104,46 @@ function CompaniesList() {
                         {companies.map((company) => (
                             <tr key={company.id}>
                                 <td>{company.id}</td>
-                                <td>{company.isVerified ? 'Oui' : 'Non'}</td>
-                                <td>{company.manager}</td>
-                                <td>{company.name}</td>
-                                <td style={{width: '20%'}}>{company.description}</td>
-                                <td>
+                                <td style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                                    <input type="checkbox" name="verified" id="verified" checked={company.isVerified} disabled/>
                                     {
                                         company.isVerified 
-                                        ? <button onClick={() => handleVerifCompany(company.id, company.isVerified)}>
-                                            Réfuté
-                                        </button>
-                                        : <button onClick={() => handleVerifCompany(company.id, company.isVerified)}>
-                                            Vérifier
-                                        </button>
+                                            ? <button onClick={() => handleVerifCompany(company.id, company.isVerified)}>
+                                                Réfuté
+                                            </button>
+                                            : <button onClick={() => handleVerifCompany(company.id, company.isVerified)}>
+                                                Vérifier
+                                            </button>
+                                    }
+                                </td>
+                                <td>{company.manager}</td>
+                                <td>
+                                    {
+                                        beingEdited && currentCompanyId === company.id
+                                            ? <input type="text" name="name" defaultValue={company.name} />
+                                            : company.name
+                                    }
+                                </td>
+                                <td style={{width: '20%'}}>
+                                    {
+                                        beingEdited && currentCompanyId === company.id
+                                        ? <textarea type="text" name="description" defaultValue={company.description} />
+                                        : company.description
+                                    }
+                                </td>
+                                <td>
+                                    {
+                                        beingEdited && currentCompanyId === company.id
+                                            ? <button onClick={() => onSave(company.id)}>
+                                                Enregistrer
+                                            </button>
+                                            : <button onClick={() => onEdit(company.id)}>
+                                                Modifier
+                                            </button>
                                     }
                                 </td>
                             </tr>
                         ))}
-                        {/* TODO */}
-                        ajouter la possibilité de modifier la description et le nom de la company
                     </tbody>
                 </table>
             }
