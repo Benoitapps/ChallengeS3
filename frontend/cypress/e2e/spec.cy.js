f9ace5cb03eb6be6d9e58d92bad882eada77b81b
Nicolas CAPELLA <colas.capel73@gmail.com>
2024-01-29 19:26:26 +0100

    cypress test

--- frontend/cypress/e2e/spec.cy.js
+++ frontend/cypress/e2e/spec.cy.js
@@ -1,5 +1,34 @@
-describe('My First Test', () => {
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+})
+
+describe('My First Test !', () => {
     it('Does not do much!', () => {
         expect(true).to.equal(true)
+    })
+})
+
+describe('Simple test', () => {
+    it('Login', () => {
+        cy.visit('http://localhost:5173')
+        cy.contains('Se connecter')
+        cy.contains('Se connecter').click()
+        cy.url().should('include', '/login')
+        cy.get('#email').type('user@user.fr')
+        cy.get('#password').type('test')
+        cy.get('input[type="submit"]').click()
+    })
+
+    it('Sign up', () => {
+        cy.visit('http://localhost:5173')
+        cy.contains('Inscription')
+        cy.contains('Inscription').click()
+        cy.url().should('include', '/signup')
+        cy.get('#firstname').type('user')
+        cy.get('#lastname').type('user')
+        cy.get('#email').type('userTest@user.fr')
+        cy.get('#password').type('test')
+        cy.get('#passwordConfirm').type('test')
+        cy.get('#Client input[type="submit"]').click()
     })
 })
