aced1dac988c729d73e85dbabfe787da8bf181ed
Nicolas CAPELLA <colas.capel73@gmail.com>
2024-02-13 21:30:03 +0100

    Update login and signup buttons in NavBar component

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -127,12 +127,12 @@ function NavBar({ isConnected, handleDisconnect, isAdmin, isManager, isCoach }) 
                 ) : (
                   <>
                     <li>
-                      <Link to="/login">
+                      <Link to="/login" id="login">
                         {t('LogIn')}
                       </Link>
                     </li>
                     <li>
-                      <Link to="/signup" className="signup">
+                      <Link to="/signup" className="signup" id="signup">
                         {t('SignUp')}
                       </Link>
                     </li>
