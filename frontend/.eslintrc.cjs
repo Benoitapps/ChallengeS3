13090c6bbf7133f6e803a2789418c4f3ae309738
decarli <benoitdecarli77@gmail.com>
2024-02-15 00:26:40 +0100

    linter

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "plugin:react/recommended",
+    "extends": ["plugin:react/recommended"],
     "overrides": [
         {
             "env": {
@@ -21,9 +21,14 @@ module.exports = {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
-    "plugins": [
-        "react"
-    ],
+    "plugins": ["react"],
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    },
     "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
     }
-}
+};
